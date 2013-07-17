1f57fd80e89f1f92e253b26e2c11997f376174cc
Julian Ozen <julian.ozen@me.com>
2013-07-17 04:55:04 +0000

    fixed buffer

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 
+var fs = require('fs');
 var app = express.createServer(express.logger());
-var contents = fs.readFileSync('index.html', 'utf-8');
+var contents = fs.readFileSync('index.html');
+buffer = new Buffer();
 
 app.get('/', function(request, response) {
-  response.send(contents.toString());
+  response.send(buffer.toString('utc-8', contents));
 });
 
 var port = process.env.PORT || 5000;
