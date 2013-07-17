4805eabb574d806a7d7a5525b2bf909d9818b512
Julian Ozen <julian.ozen@me.com>
2013-07-17 02:31:43 +0000

    added file read

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var contents = fs.readFile('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
