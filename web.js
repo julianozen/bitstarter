1afb35c3bb62e80aa7ffb357e3b59ebffe0bb367
Julian Ozen <julian.ozen@me.com>
2013-07-17 02:48:22 +0000

    added file read sync

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var contents = fs.readFile('index.html');
+var contents = fs.readFileSync('index.html', 'utf-8');
 
 app.get('/', function(request, response) {
   response.send(contents);
