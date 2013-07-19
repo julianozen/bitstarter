4da0c7a5e99c54d84950566de55ecf749daf2519
Julian Ozen <julian.ozen@me.com>
2013-07-19 04:27:35 +0000

    fixed?

--- grader.js
+++ grader.js
@@ -24,9 +24,11 @@ References:
 
 var fs = require('fs');
 var program = require('commander');
-var cheerio = require('cheerio');
+var cheerio = require('cheerio');  
 var HTMLFILE_DEFAULT = "index.html";
-var CHECKSFILE_DEFAULT = "checks.json";
+var CHECKSFILE_DEFAULT = "checks.json"; 
+var URL_DEFAULT = "https://spark-public.s3.amazonaws.com/startup/code/bitstarter.html"
+var rest= require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -66,8 +68,13 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url<url_link>', 'URL to index.html', clone(assertFileExists),URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var fileused = program.file;
+    if (fileused == HTMLFILE_DEFAULT){
+	filedused = rest.get(program.url);
+    }
+    var checkJson = checkHtmlFile(fileused, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
